b1da36a2339752c5d2887d634eef417fa510405c
yanlinbo <973664322@qq.com>
2022-11-14 22:57:20 +0800

    yanlinbo 20221113 两数之和-二分查找和双指针算法

--- algorithm/src/com/ylb/array/TwoNumSum.java
+++ algorithm/src/com/ylb/array/TwoNumSum.java
@@ -15,6 +15,10 @@ public class TwoNumSum {
         System.out.println(Arrays.toString(solution01(new int[]{1,2,3,4,5,6},10)));
 
         System.out.println(Arrays.toString(solutionNew(new int[]{1,2,3,4,5,6},10)));
+
+        System.out.println(Arrays.toString(twoSearch(new int[]{1,2,3,4,5,6},10)));
+
+        System.out.println(Arrays.toString(twoPoint(new int[]{1,2,3,4,5,6},10)));
     }
 
     /**
@@ -70,6 +74,58 @@ public class TwoNumSum {
             }
             //注意这里别写错了
             map.put(ints[j],j);
+        }
+
+        return new int[0];
+    }
+
+    /**
+     * 二分查找算法
+     * @param ints
+     * @param target
+     * @return
+     */
+    private static int[] twoSearch(int[] ints, int target) {
+
+        for (int j = 0; j < ints.length; j++) {
+            int low = j, high = ints.length - 1;
+            //todo 注意这里不要漏掉“=”  想想为什么？
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(ints[mid] == target - ints[j]){
+                    return new int[]{j,mid};
+                }else if(ints[mid] > target - ints[j]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+
+    /**
+     * 双指针算法
+     * @param ints
+     * @param target
+     * @return
+     */
+    private static int[] twoPoint(int[] ints, int target) {
+
+        for (int j = 0; j < ints.length; j++) {
+            int low = j, high = ints.length - 1;
+            while(low < high){
+                int sum = ints[low] + ints[high];
+                if(sum == target){
+                    return new int[]{low,high};
+                }else if(sum > target){
+                    high--;
+                }else{
+                    low++;
+                }
+            }
         }
 
         return new int[0];
